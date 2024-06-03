34fd7c3494588f30477799382d2a4776eefbffeb
ijoks <168131581+ijoks@users.noreply.github.com>
2024-06-03 13:58:46 +0300

    Update generate_image.py

--- generate_image.py
+++ generate_image.py
@@ -4,19 +4,19 @@ from rdkit.Chem.rdMolDescriptors import CalcMolFormula
 
 def generate_chemical_image(compound_name, output_path):
     try:
-        # Convert the compound name to a molecule
+        # Muudab aine nime üheks aine molekuliks
         mol = Chem.MolFromSmiles(compound_name)
         if mol is None:
             return False, None
         
-        # Generate the image
-        img = Draw.MolToImage(mol)
-        img.save(output_path)
         
-        # Calculate the molecular formula
-        molecular_formula = CalcMolFormula(mol)
+        img = Draw.MolToImage(mol) # Loob aine molekulist pildi
+        img.save(output_path) # Salvestab pildi static/images kausta
         
-        return True, molecular_formula
+        
+        molecular_formula = CalcMolFormula(mol) # Arvutab brutovalemi
+        
+        return True, molecular_formula 
     except Exception as e:
         print(f"Error generating image for {compound_name}: {e}")
         return False, None
